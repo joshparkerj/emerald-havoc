a5e5eb82e353a7ffe9ce2529920d26c673707384
Josh Parker <joshBBparker@gmail.com>
2022-03-06 00:38:20 -0700

    add name columns to migrations

--- database/migrations/2022_03_06_064746_create_videos_table.php
+++ database/migrations/2022_03_06_064746_create_videos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
